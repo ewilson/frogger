4d5ae07687ef5b2609892803f09d85d6d1ab5eee
Eric Wilson <eric.wilson02@cardinalhealth.com>
2019-04-27 18:03:50 -0400

    Draw Road and Score Area

--- frogger.py
+++ frogger.py
@@ -2,6 +2,7 @@ import arcade
 
 SCREEN_HEIGHT = 560
 SCREEN_WIDTH = 800
+ROAD_SECTION_HEIGHT = SCREEN_HEIGHT / 8
 
 
 class Frogger(arcade.Window):
@@ -13,10 +14,29 @@ class Frogger(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
+        self._build_road()
+        self._draw_score_area()
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.ESCAPE:
             exit()
+
+    def _build_road(self):
+        arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, 6 * ROAD_SECTION_HEIGHT, ROAD_SECTION_HEIGHT,
+                                          arcade.color.BLACK)
+
+        def _stripe_road(height, offset=0):
+            for x in range(0, SCREEN_WIDTH + 50, 100):
+                arcade.draw_rectangle_filled(x + offset, height, 40, 2, arcade.color.WHITE)
+
+        _stripe_road(ROAD_SECTION_HEIGHT * 2, 50)
+        _stripe_road(ROAD_SECTION_HEIGHT * 3)
+        _stripe_road(ROAD_SECTION_HEIGHT * 4, 50)
+        _stripe_road(ROAD_SECTION_HEIGHT * 5)
+
+    def _draw_score_area(self):
+        arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT, 7 * ROAD_SECTION_HEIGHT, arcade.color.BLACK)
+        arcade.draw_text("Score: ", 10, SCREEN_HEIGHT - 40, arcade.color.WHITE, 20)
 
 
 window = Frogger()
